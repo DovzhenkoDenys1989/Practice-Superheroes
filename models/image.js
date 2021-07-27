bf82aba7eb13caf582c2c3a8358b63ac8c2019ba
Denis Dovzhenko <ddv10jan1989@gmail.com>
2021-07-27 12:26:49 +0300

    Add models and migrations Image

---
+++ models/image.js
@@ -0,0 +1,36 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Image extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Image.init({
+    heroId: {
+      field: 'hero_id',
+      allowNull: false,
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'superheroes',
+        key: 'id'
+      }
+    },
+    imagePath: {
+      field: 'image_path',
+      allowNull: false,
+      type: DataTypes.TEXT
+    }
+  }, {
+    sequelize,
+    modelName: 'Image',
+  });
+  return Image;
+};
